109193c9c028e2ab64ba303372dab62c58e4f761
Zach Cervi <zachcervi@gmail.com>
2022-06-07 09:03:42 -0600

    updated settings added toggle to restart onboarding screen

--- Fruits/App/SettingsView.swift
+++ Fruits/App/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingsView: View {
     //MARK: PROPERTIES
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     //MARK: BODY
     var body: some View {
@@ -34,6 +35,34 @@ struct SettingsView: View {
                         }
                     }
                     //MARK: - SECTION 2
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ){
+                        Divider().padding(.vertical, 4)
+                        Text("If you wish, you can restart the application by toggling the switch. That way it restarts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding){
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                    }//: GROUPBOX
                     
                     //MARK: - SECTION 3
                     GroupBox(
